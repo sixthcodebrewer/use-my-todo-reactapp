1fec0e76bcdf5c6e3e70b98215e2b5ac7908b3c3
Varun j <48326355+sixthcodebrewer@users.noreply.github.com>
2020-08-15 21:12:44 +0530

    added all

---
+++ build/precache-manifest.e0581b81503081b0b79b3d5283c6bcd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031419bef91e028b55440ec2f8a94007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723370183364032f7d9a",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "306cbb52e803d14c583b",
+    "url": "/static/js/2.1fba836f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fba836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723370183364032f7d9a",
+    "url": "/static/js/main.9b44eaf2.chunk.js"
+  },
+  {
+    "revision": "53f8148c98f1c9cda1ba",
+    "url": "/static/js/runtime-main.a643b587.js"
+  }
+]);
